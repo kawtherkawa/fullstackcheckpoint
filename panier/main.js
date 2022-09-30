01045a39eb51eb382758b03ad17b803ee84a878d
kawtherkawa <kaouther.braiek@gmail.com>
2022-09-30 12:15:11 +0100

    this second comment

---
+++ panier/main.js
@@ -0,0 +1,46 @@
+var btnsfavs= Array.from(document.querySelectorAll('.coe'))
+var btnsdelete= Array.from(document.querySelectorAll('.sup'))
+var btnsplus=Array.from(document.querySelectorAll('.plus'))
+var btnsmoins=Array.from(document.querySelectorAll('.moins'))
+var  products=Array.from(document.querySelectorAll('.cat'))
+//console.log(btnsplus);
+console.log(btnsfavs);
+ for( let btnfav of btnsfavs )
+ {
+    btnfav.addEventsListner("click", function(){
+    if( btnfav.style.color=="black"){
+        btnfav.style.color="red"
+    }
+    else{
+        btnfav.style.color=="black"
+    }
+})
+
+}
+
+for(i in btnsdelete) 
+(
+btnsdelete[i].addEventsListner("click",function(){
+    products[i].remove()
+}))
+
+  for(let btnplus of btnsplus)
+
+  {
+    btnplus.addEventListener("click",function(){
+        btnplus.nextElementSibling.innerHTML++ 
+        
+    }
+    )
+}
+
+  for(let btnmoins of btnsmoins)
+
+  {
+    btnmoins.addEventListener("click",function(){
+        btnmoins.previousElementSibling.innerHTML > 0 ?
+        btnmoins.previousElementSibling.innerHTML -- :
+        null
+    }
+    )
+}
